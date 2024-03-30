77f7b93dc5de16fc32bdbcdf4738cd6682094788
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-03-30 22:50:21 +0100

    feat: add seoMeta

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'First Committer',
+  ogTitle: 'First Committer',
+  description: 'Find the first commit of any public GitHub repository 🕵️‍♂️.',
+  ogDescription:
+    'A tool that lets you find the first commit(ter) behind any public GitHub repository.',
+  ogImage:
+    'https://raw.githubusercontent.com/khalidbelk/FirstCommitter/main/assets/logo/LogoText.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
